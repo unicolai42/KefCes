4695e4542f0f032f0370cda1477be7830fc21713
UgoNic <ugo.nicolai@gmail.com>
2019-03-17 22:42:25 +1100

    glitch almost done + start portofolio

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -42,18 +42,20 @@ class Home extends React.Component {
     return (
       <React.Fragment>
         <div id='Home_Background'>
-            <img className='Home_KefCes' id='Home_Ugo' src="/ugo.png" alt="growth profile"/>
-            <img className='Home_KefCes' id='Home_Alice' src="/alice.png" alt="storytelling profile"/>
-            <div id='Home_Frame'>
-                <div id='Home_Baseline'>SCALE YOUR PROJECT</div>
-                <div id='Home_Description'>StoryTelling, Editing, Video, Photo, Design, Growth Hacking, Website, Automation. </div>
-                <div id='Home_CTAFrame'>
-                  <div onClick={() => {$crisp.push(['do', 'chat:open'])}} id='Home_CTA'>WORK WITH US</div>
-                </div>
-            </div>
+          <div id='Home_Filter'></div>
+          <img className='Home_KefCes' id='Home_Ugo' src="/ugo.png" alt="growth profile"/>
+          <img className='Home_KefCes' id='Home_Alice' src="/alice_home.gif" alt="storytelling profile"/>
+          <div id='Home_Frame'>
+              <div id='Home_Baseline'>SCALE YOUR PROJECT</div>
+              <div id='Home_Description'>StoryTelling, Editing, Video, Photo, Design, Growth Hacking, Website, Automation.</div>
+              <div id='Home_CTAFrame'>
+                <div onClick={() => {$crisp.push(['do', 'chat:open'])}} id='Home_CTA'>WORK WITH US</div>
+              </div>
+          </div>
         </div>
         <div id='Portofolio_Background'>
-          
+          <div id='Portofolio_FrameUgo'></div>
+          <div id='Portofolio_FrameAlice'></div>
         </div>
         <div id='About_Background'>
         {/* <video width="1000" height="1000" autoPlay muted loop onEnded={() => { console.log('Ended Video') }}>
@@ -167,8 +169,8 @@ class Home extends React.Component {
   }
 }
 
-const mindsetTxt = 'Le monde n’a jamais changé aussi vite qu’aujourd’hui. Internet permet à qui le veut d’apprendre ce qu’il veut, quand il veut, où il veut.\n\nFace à ce constat, on a décidé de partir découvrir le monde et inspirer aà travers notre chaine youtube d\'autres personnes qui voudraient aà leur tour voyager et travailler en même temps.'
-const descriptionAlice = 'Le monde n’a jamais changé aussi vite qu’aujourd’hui. Internet permet à qui le veut d’apprendre ce qu’il veut, quand il veut, où il veut.'
-const descriptionUgo = 'Le monde n’a jamais changé aussi vite qu’aujourd’hui. Internet permet à qui le veut d’apprendre ce qu’il veut, quand il veut, où il veut.'
+const mindsetTxt = 'Avec le réchauffement climatique la fonte du permafrost s’accélère.\n60% des animaux sauvages sur Terre ont disparu en moins de 50 ans.\nUn tier de la nourriture que l’on produit chaque année part à la poubelle.\n1% de la population détient 50% de la richesse mondiale.\n\nBref on a conscience que c’est la merde et on veut aider des entreprises qui se bougent à changer les choses plus rapidement.'
+const descriptionAlice = 'Le monde n’a jamais changé aussi vite qu’aujourd’hui. Internet permet à qui le veut d’apprendre ce qu’il veut, quand il veut, où il veut.\n\nLe monde n’a jamais changé aussi vite qu’aujourd’hui. Internet permet à qui le veut d’apprendre ce qu’il veut, quand il veut, où il veut.'
+const descriptionUgo = 'Le monde n’a jamais changé aussi vite qu’aujourd’hui. Internet permet à qui le veut d’apprendre ce qu’il veut, quand il veut, où il veut.\n\nLe monde n’a jamais changé aussi vite qu’aujourd’hui. Internet permet à qui le veut d’apprendre ce qu’il veut, quand il veut, où il veut.'
 
 export default Home
